d629a9ff9c8579176f820b3f0923845cd8fd2b0c
Alan Smithee <alan.smithee@example.com>
2016-01-26 10:03:48 +0100

    Commit *all* the shit!

--- assets/js/field_locale.js
+++ assets/js/field_locale.js
@@ -1,7 +1,10 @@
+/* global $ */
 $(function() {
 	// Prepend translation icons to translatable fields.
-	$($('#translatable-fields').val()).closest('fieldset').find('label').prepend('<i class="fa fa-language icon"></i> ');
-
+	$($('#translatable-fields').val()).closest('fieldset').each(function(i, el){
+		$(el).find('label').first().prepend('<i class="fa fa-language icon"></i> ');
+	})
+	
 	// Move locale switcher to tab navigation
 	$("#locale-select").detach().appendTo('#filtertabs');
 
@@ -23,27 +26,29 @@ $(function() {
 			.addClass('disabled')
 			.siblings()
 			.removeClass('disabled');
-
+		
+		var boltField = $('[data-bolt-field="slug"]');
+		var fconf = boltField.data('bolt-fconf');
+		$.each(fconf.uses, function (i, name) {
+			$('#' + name).off('propertychange.bolt input.bolt change.bolt');
+		});
+		
 		$.get(el.attr('href'), {
-				locale: locale,
-				content_type: $('#contenttype').val(),
-				content_type_id: $('#id').val()
-			}, function(data) {
-				$.each(data, function() {
-					setValue(this.field, this.value);
-				})
-
-				$('#locale').val(locale);
-				lockInputFields(false);
+			locale: locale,
+			content_type: $('#contenttype').val(),
+			content_type_id: $('#id').val()
+		}, function(data) {
+			$.each(data, function() {
+				setValue(this.field, this.value);
 			})
-			.fail(function() {
-				// show warning
-				lockInputFields(false);
-			});
-	});
-
-	$('#locale').change(function() {
-		alert('yo');
+			$('#locale').val(locale);
+			lockInputFields(false);
+			//Bolt.app.run()
+		})
+		.fail(function() {
+			// show warning
+			lockInputFields(false);
+		});
 	});
 });
 
@@ -55,18 +60,154 @@ function lockInputFields(status) {
 }
 
 function setValue(field, value) {
-	var el = $('#' + field);
-
-	switch (el.getType()) {
+	var el = $('#' + field + ', #field-' + field + ', [data-bolt-field="' + field + '"], #' + field + '-video, #gridfield-' + field);
+	var parent = el.closest('.form-group[data-fieldtype]');
+	if(parent.length == 0 && field == 'seo'){
+		parent = $('#seo').closest('.tab-pane').find('[data-fieldtype="seo"]')
+	}
+	var type = parent.attr('data-fieldtype');
+	var boltField = $('[data-bolt-field="slug"]');
+	var fconf = boltField.data('bolt-fconf');
+	switch (type) {
 		case 'textarea':
-			if(el.hasClass('ckeditor')) {
-				CKEDITOR.instances[el.attr('name')].setData(value);
-			} else {
-				el.html(value);
+		case 'text':
+			el.val(value);
+			break;
+		case 'html':
+			CKEDITOR.instances[el.attr('name')].setData(value);
+			break;
+		case 'markdown':
+			parent.find('.CodeMirror').get(0).CodeMirror.setValue(value);
+			break;
+		case 'checkbox':
+			if(value == 1){
+				el.prop('checked', true);
+			}else{
+				el.prop('checked', false);
 			}
 			break;
-		default:
+		case 'slug':
+			if (value.trim() == ''){
+				fconf.isEmpty = true;
+			} else {
+				fconf.isEmpty = false;
+				parent.find('.input-group').removeClass('unlocked').addClass('locked');
+				$.each(fconf.uses, function (i, name) {
+					$('#' + name).off('propertychange.bolt input.bolt change.bolt');
+				});
+			}
+			boltField.get(0).dataset.boltFconf = JSON.stringify(fconf);
+			parent.find('input').val(value);
+			parent.find('em').html(value);
+			Bolt.fields.slug.init(boltField, fconf);
+			break;
+		case 'datetime':
+		case 'date':
 			el.val(value);
+			Bolt.datetime.update();
+			break;
+		case 'select':
+			if(fconf.uses.indexOf(field)){
+				el.off('propertychange.bolt input.bolt change.bolt');
+			}
+			if(el.attr('multiple')){
+				try {
+					el.val(JSON.parse(value))
+				} catch (e) {
+					console.log('not valid json for field ' + field)
+				}
+			}else{
+				el.val(value);
+			}
+			break;
+		case 'geolocation':
+			parent.find('input').val('');
+			try {
+				value = JSON.parse(value);
+				for (var key in value) {
+					$('[name="'+field+'['+key+']"]').val(value[key]).trigger('input')
+				}
+			} catch (e) {
+				console.log('not valid json for field ' + field)
+			}
+			break;
+		case 'image':
+			parent.find('input').val('');
+			try {
+				value = JSON.parse(value);
+				for (var key in value) {
+					$('[name="'+field+'['+key+']"]').val(value[key])
+				}
+				$('#thumbnail-' + field).html('<img src="' + Bolt.conf('paths.root') + 'thumbs/200x150c/' +
+                            encodeURI(value.file) + '" width="200" height="150">');
+			} catch (e) {
+				parent.find('.content-preview img').attr('src','/app/view/img/default_empty_4x3.png')
+				console.log('not valid json for field ' + field)
+			}
+			break;
+		case 'video':
+			try {
+				value = JSON.parse(value);
+				$('[name="'+field+'[url]"]').val(value.url).trigger('input');
+			} catch (e) {
+				$('[name="'+field+'[url]"]').val('').trigger('input');
+				console.log('not valid json for field ' + field)
+			}
+			break;
+		case 'filelist':
+		case 'imagelist':
+			Bolt[type][field].list.reset();
+			Bolt[type][field].render();
+			try {
+				value = JSON.parse(value);
+				for (var key in value) {
+					Bolt[type][field].add(value[key].filename, value[key].title)
+				}
+			} catch (e) {
+				console.log('not valid json for field ' + field)
+			}
+			
+			break;
+		case 'seo':
+			try {
+				value = JSON.parse(value);
+				$.each(value, function (subindex, subitem) {
+					$('#seofields-' + subindex).val(subitem).trigger('input').trigger('blur');
+				});
+				el.val(value).trigger('input').trigger('blur');
+			} catch (e) {
+				console.log('Error on SEO field: ', field, value);
+				el.val(value);
+			}
+			break;
+		case 'grid':
+			el.val(value);
+			document.getElementById('hot-'+field).hot.loadData(JSON.parse(value));
+			break;
+		case 'faiconpicker':
+			el.val(value);
+			parent.find('[title=".'+value+'"]').trigger('click');
+			break;
+		case 'bootstrapcolorpicker':
+			el.val(value);
+			el.colorpicker('setValue', value);
+			break;
+		default:
+			console.log(el, parent, type, field, value)
+			try {
+				value = JSON.parse(value);
+				if ($.type(value) === 'object') {
+					$.each(value, function (subindex, subitem) {
+						$(':input[name="' + field + '[' + subindex + ']"]').val(subitem).trigger('input').trigger('blur');
+					});
+				}else{
+					console.log('Unkown field with non-objects, trying to set with val: ', field, value);
+					el.val(value).trigger('input').trigger('blur');
+				}
+			} catch (e) {
+				el.val(value).trigger('input').trigger('blur');
+			}
+			break;
 	}
 }
 
---
+++ src/Frontend/LocalizedFrontend.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Bolt\Extension\Animal\Translate;
+
+
+Class LocalizedFrontend extends \Bolt\Controllers\Frontend
+{
+    public function record(\Silex\Application $app, $contenttypeslug, $slug = '')
+    {
+        $contenttype = $app['storage']->getContentType($contenttypeslug);
+
+        // If the contenttype is 'viewless', don't show the record page.
+        if (isset($contenttype['viewless']) && $contenttype['viewless'] === true) {
+            return $app->abort(Response::HTTP_NOT_FOUND, "Page $contenttypeslug/$slug not found.");
+        }
+
+        // Perhaps we don't have a slug. Let's see if we can pick up the 'id', instead.
+        if (empty($slug)) {
+            $slug = $app['request']->get('id');
+        }
+
+        $slug = $app['slugify']->slugify($slug);
+
+        // First, try to get it by slug.
+        $content = $app['storage']->getContent($contenttype['slug'], array('slug' => $slug, 'returnsingle' => true, 'log_not_found' => !is_numeric($slug)));
+
+        if (!$content && !is_numeric($slug)) {
+            // And otherwise try getting it by translated slugs
+            $extension  = new \Bolt\Extension\Animal\Translate\Extension($app);
+            $match = $extension->matchSlug($contenttype['slug'], $slug);
+            if(!empty($match)){
+                $content = $app['storage']->getContent($contenttype['slug'], array('id' => $match['content_type_id'], 'returnsingle' => true));
+            }
+        }
+        if (!$content && is_numeric($slug)) {
+            // And otherwise try getting it by ID
+            $content = $app['storage']->getContent($contenttype['slug'], array('id' => $slug, 'returnsingle' => true));
+        }
+
+        // No content, no page!
+        if (!$content) {
+            return $app->abort(Response::HTTP_NOT_FOUND, "Page $contenttypeslug/$slug not found.");
+        }
+
+        // Then, select which template to use, based on our 'cascading templates rules'
+        $template = $app['templatechooser']->record($content);
+
+        $paths = $app['resources']->getPaths();
+
+        // Setting the canonical URL.
+        if ($content->isHome() && ($template == $app['config']->get('general/homepage_template'))) {
+            $app['resources']->setUrl('canonicalurl', $paths['rooturl']);
+        } else {
+            $url = $paths['canonical'] . $content->link();
+            $app['resources']->setUrl('canonicalurl', $url);
+        }
+
+        // Setting the editlink
+        $app['editlink'] = Lib::path('editcontent', array('contenttypeslug' => $contenttype['slug'], 'id' => $content->id));
+        $app['edittitle'] = $content->getTitle();
+
+        // Make sure we can also access it as {{ page.title }} for pages, etc. We set these in the global scope,
+        // So that they're also available in menu's and templates rendered by extensions.
+        $app['twig']->addGlobal('record', $content);
+        $app['twig']->addGlobal($contenttype['singular_slug'], $content);
+
+        // Render the template and return.
+        return $this->render($app, $template, $content->getTitle());
+    }
+        
+    public function homepageRedirect(\Silex\Application $app){
+        return $app->redirect(reset($app['config']->get('general/locales'))['slug']);
+    }
+}
