ee9e4d63a246d2fbde78a1da6e010baa388db49e
Alan Smithee <alan.smithee@example.com>
2016-06-29 10:14:20 +0200

    Boostrapping 3.0 with guenther

--- init.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Bolt\Extension\Animal\Translate\Extension;
-
-$app['extensions']->register(new Extension($app));
---
+++ src/TranslateExtension.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bolt\Extension\Animal\Translate;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * Translate extension class.
+ *
+ * @author Your Name <you@example.com>
+ */
+class TranslateExtension extends SimpleExtension
+{
+
+}
