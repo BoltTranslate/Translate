518acd62b59c4a9130fc44e297da375358de39e6
Ross Riley <riley.ross@gmail.com>
2017-07-20 21:25:00 +0100

    extend FieldTypeBase so the new fields implement the new interface

--- src/Field/LocaleField.php
+++ src/Field/LocaleField.php
@@ -3,8 +3,9 @@
 namespace Bolt\Extension\Animal\Translate\Field;
 
 use Bolt\Storage\Field\FieldInterface;
+use Bolt\Storage\Field\Type\FieldTypeBase;
 
-class LocaleField implements FieldInterface
+class LocaleField extends FieldTypeBase implements FieldInterface
 {
     public function getName()
     {
