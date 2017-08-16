ca8bec95487d3980333c8bfa96a036dc40b3be3c
neal marques <neal.marques@gmail.com>
2017-08-16 10:25:28 +0100

    Catch when record['template'] not defined

--- src/Storage/Legacy.php
+++ src/Storage/Legacy.php
@@ -86,7 +86,11 @@ class Legacy extends Storage
 			if ($localeData !== null) {
 				foreach ($localeData as $key => $value) {
 					if ($key === 'templatefields') {
-						$templateFields = $app['config']->get('theme/templatefields/' . ($values['template'] == Null ? $contentType["record_template"] : $values['template']) . '/fields');
+						if (is_set($record['template']) && $record['template']==Null) {
+							$templateFields = $app['config']->get('theme/templatefields/' .  $contentType['record_template'] . '/fields');
+						} else {
+							$templateFields = $app['config']->get('theme/templatefields/' . $record['template'] . '/fields');
+						}
 						if ( is_array($templateFields) ) {
 							foreach ($templateFields as $key => $field) {
 								if ($field['type'] === 'repeater') {
